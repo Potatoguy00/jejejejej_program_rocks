30650604eea1dbd125e66975fbf4bfc3d5216772
Potatoguy00 <elias.paulsson-febo@elev.ga.ntig.se>
2025-08-25 09:23:02 +0200

    Starting the project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+      Console.Write("How many times? ");
+      int number = int.Parse(Console.ReadLine());
+
+      for (int i = 0; i < number; i++)
+        foreach (string car in cars)
+          Console.WriteLine(car);
+    }
+  }
+}
