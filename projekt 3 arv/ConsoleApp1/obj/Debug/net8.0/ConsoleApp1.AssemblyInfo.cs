39f413df17573a130ba0f85e95feb1c5c931a4bf
Potatoguy00 <elias.paulsson-febo@elev.ga.ntig.se>
2025-09-11 14:02:06 +0200

    weqwek

--- projekt 3 arv/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ projekt 3 arv/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c90e0cfc1e0f95a5372f66af2b932b83b5bd2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac56cdf1bd577bebac23f4fe344d3736b4bd374")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
